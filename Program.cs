41f087e72daeaab433fb7e679cb67f92359e11fe
Nathália Carvalho <137252066+nathycarvalho13@users.noreply.github.com>
2024-08-04 05:02:51 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,59 +1,134 @@
-﻿using naty;
+using System;
+using ContrachequeApp;
 using Newtonsoft.Json;
-using System.Xml;
 
-var funcionario = new Funcionario();
-
-while (true)
+class Program
 {
-    PerguntaNome();
-    PerguntaSalario();
-    PerguntaTotalHorasExtras();
-
-    if (funcionario.HorasExtras > 0)
+    static void Main(string[] args)
     {
-        EscreverLinha("Digite o valor das horas extras: ");
-        funcionario.ValorHorasExtras = Convert.ToDecimal(Console.ReadLine());
+        var funcionario = new Funcionario();
+
+        while (true)
+        {
+            PerguntaInformacoesEmpregado(funcionario);
+            Console.Clear();
+            PerguntaInformacoesContracheque(funcionario);
+            Console.Clear();
+            PerguntaBeneficios(funcionario);
+            Console.Clear();
+
+            funcionario.ExibirContracheque();
+
+            EscreverLinha("Deseja salvar o contracheque? (s/n): ");
+            EscreverLinha("");
+
+            if (Console.ReadLine().ToLower() == "s")
+                SalvarContracheque(funcionario);
+
+            EscreverLinha("Deseja cadastrar um novo contracheque? (s/n): ");
+            EscreverLinha("");
+
+            if (Console.ReadLine().ToLower() != "s")
+                break;
+
+            Console.Clear();
+        }
+    }
+
+    static void PerguntaInformacoesEmpregado(Funcionario funcionario)
+    {
+        EscreverLinha("Informações do Empregado");
+        EscreverLinha("Digite o nome do funcionário: ");
+        funcionario.Nome = Console.ReadLine();
+        EscreverLinha("Digite o número de identificação (CPF ou RG): ");
+        funcionario.Identificacao = Console.ReadLine();
+        EscreverLinha("Digite o cargo/função do funcionário: ");
+        funcionario.Cargo = Console.ReadLine();
+        EscreverLinha("Digite a matrícula/número de registro do funcionário: ");
+        funcionario.Matricula = Console.ReadLine();
+        EscreverLinha("Digite a data de admissão (dd/mm/aaaa): ");
+        funcionario.DataAdmissao = Console.ReadLine();
+        EscreverLinha("Digite o departamento/setor do funcionário: ");
+        funcionario.Departamento = Console.ReadLine();
         EscreverLinha("");
     }
 
-    funcionario.ExibirContracheque();
+    static void PerguntaInformacoesContracheque(Funcionario funcionario)
+    {
+        EscreverLinha("Informações do Contracheque");
+        EscreverLinha("Digite o salário do funcionário: ");
+        funcionario.Salario = Convert.ToDecimal(Console.ReadLine());
+        EscreverLinha("Digite o total de horas extras: ");
+        funcionario.HorasExtras = Convert.ToDecimal(Console.ReadLine());
 
-    EscreverLinha("Deseja salvar o contracheque? (s/n): ");
-    EscreverLinha("");
+        if (funcionario.HorasExtras > 0)
+        {
+            EscreverLinha("Digite o valor das horas extras: ");
+            funcionario.ValorHorasExtras = Convert.ToDecimal(Console.ReadLine());
+        }
 
-    if (Console.ReadLine().ToLower() == "s")
-        SalvarContracheque(funcionario);
+        EscreverLinha("Você trabalha no turno da noite? (s/n): ");
+        if (Console.ReadLine().ToLower() == "s")
+        {
+            EscreverLinha("Digite o valor do adicional noturno: ");
+            funcionario.AdicionalNoturno = Convert.ToDecimal(Console.ReadLine());
+            EscreverLinha("Digite o valor do adicional de insalubridade: ");
+            funcionario.AdicionalInsalubridade = Convert.ToDecimal(Console.ReadLine());
+            EscreverLinha("Digite o valor do adicional de periculosidade: ");
+            funcionario.AdicionalPericulosidade = Convert.ToDecimal(Console.ReadLine());
+        }
 
-    EscreverLinha("Deseja cadastrar um novo contracheque? (s/n): ");
-    EscreverLinha("");
+        EscreverLinha("A empresa oferece comissões, bônus ou PLR? (s/n): ");
+        if (Console.ReadLine().ToLower() == "s")
+        {
+            EscreverLinha("Digite o valor das comissões: ");
+            funcionario.Comissoes = Convert.ToDecimal(Console.ReadLine());
+            EscreverLinha("Digite o valor dos bônus e gratificações: ");
+            funcionario.BonusGratificacoes = Convert.ToDecimal(Console.ReadLine());
+            EscreverLinha("Digite o valor da participação nos lucros e resultados (PLR): ");
+            funcionario.PLR = Convert.ToDecimal(Console.ReadLine());
+        }
 
-    if (Console.ReadLine().ToLower() != "s")
-        break;
+        EscreverLinha("Digite o valor da ajuda de custo: ");
+        funcionario.AjudaCusto = Convert.ToDecimal(Console.ReadLine());
+        EscreverLinha("Digite o valor do vale transporte: ");
+        funcionario.ValeTransporte = Convert.ToDecimal(Console.ReadLine());
+        EscreverLinha("Digite o valor do vale refeição: ");
+        funcionario.ValeRefeicao = Convert.ToDecimal(Console.ReadLine());
+        EscreverLinha("Digite o valor do plano de saúde pago pelo empregador: ");
+        funcionario.PlanoSaudeEmpregador = Convert.ToDecimal(Console.ReadLine());
+        EscreverLinha("Digite o valor do plano odontológico pago pelo empregador: ");
+        funcionario.PlanoOdontologicoEmpregador = Convert.ToDecimal(Console.ReadLine());
+        EscreverLinha("Digite o valor do seguro de vida pago pelo empregador: ");
+        funcionario.SeguroVida = Convert.ToDecimal(Console.ReadLine());
+        EscreverLinha("Digite o valor do auxílio creche: ");
+        funcionario.AuxilioCreche = Convert.ToDecimal(Console.ReadLine());
+        EscreverLinha("Digite o valor do auxílio educação: ");
+        funcionario.AuxilioEducacao = Convert.ToDecimal(Console.ReadLine());
+        EscreverLinha("");
+    }
 
-    Console.Clear();
+    static void PerguntaBeneficios(Funcionario funcionario)
+    {
+        EscreverLinha("Benefícios Adicionais");
+        EscreverLinha("Digite o valor total dos benefícios: ");
+    }
+
+    static void EscreverLinha(string texto)
+    {
+        Console.WriteLine(texto);
+    }
+
+    static void SalvarContracheque(Funcionario funcionario)
+    {
+        var contrachequeJson = JsonConvert.SerializeObject(funcionario, Formatting.Indented);
+        var caminhoArquivo = "contracheque.json";
+
+        File.WriteAllText(caminhoArquivo, contrachequeJson);
+        EscreverLinha("Contracheque salvo com sucesso em " + caminhoArquivo);
+    }
 }
 
-void PerguntaNome()
-{
-    EscreverLinha("Digite o nome do funcionário: ");
-    funcionario.Nome = Console.ReadLine();
-    EscreverLinha("");
-}
-
-void PerguntaSalario()
-{
-    EscreverLinha("Digite o salário do funcionário: ");
-    funcionario.Salario = Convert.ToDecimal(Console.ReadLine());
-    EscreverLinha("");
-}
-
-void PerguntaTotalHorasExtras()
-{
-    EscreverLinha("Digite o total de horas extras: ");
-    funcionario.HorasExtras = Convert.ToDecimal(Console.ReadLine());
-    EscreverLinha("");
-}
 
 void EscreverLinha(string texto) 
 {
