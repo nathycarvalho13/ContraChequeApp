500922284275836b19b34f222e554ec0d272557f
Nathália Carvalho <nathycarvalhoflu@gmail.com>
2024-08-04 00:16:05 -0300

    Adicionar arquivos de projeto.

---
+++ Funcionario.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naty
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public decimal Salario { get; set; }
+        public decimal HorasExtras { get; set; }
+        public decimal ValorHorasExtras { get; set; }
+
+        public Funcionario(string nome, decimal salario, decimal horasExtras, decimal valorHorasExtras)
+        {
+            Nome = nome;
+            Salario = salario;
+            HorasExtras = horasExtras;
+            ValorHorasExtras = valorHorasExtras;
+        }
+
+        public Funcionario()
+        {
+
+        }
+
+        public decimal CalcularDecimoTerceiro()
+        {
+            return Salario;
+        }
+
+        public decimal CalcularFerias()
+        {
+            return Salario + (Salario / 3);
+        }
+
+        public decimal CalcularSalarioTotal()
+        {
+            return Salario + (HorasExtras * ValorHorasExtras);
+        }
+
+        public void ExibirContracheque()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Salário: {Salario:C}");
+            Console.WriteLine($"Horas Extras: {HorasExtras}h");
+            Console.WriteLine($"Valor Horas Extras: {ValorHorasExtras:C}");
+            Console.WriteLine($"Salário Total: {CalcularSalarioTotal():C}");
+            Console.WriteLine($"Décimo Terceiro: {CalcularDecimoTerceiro():C}");
+            Console.WriteLine($"Férias: {CalcularFerias():C}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using naty;
+using Newtonsoft.Json;
+using System.Xml;
+
+var funcionario = new Funcionario();
+
+while (true)
+{
+    PerguntaNome();
+    PerguntaSalario();
+    PerguntaTotalHorasExtras();
+
+    if (funcionario.HorasExtras > 0)
+    {
+        EscreverLinha("Digite o valor das horas extras: ");
+        funcionario.ValorHorasExtras = Convert.ToDecimal(Console.ReadLine());
+        EscreverLinha("");
+    }
+
+    funcionario.ExibirContracheque();
+
+    EscreverLinha("Deseja salvar o contracheque? (s/n): ");
+    EscreverLinha("");
+
+    if (Console.ReadLine().ToLower() == "s")
+        SalvarContracheque(funcionario);
+
+    EscreverLinha("Deseja cadastrar um novo contracheque? (s/n): ");
+    EscreverLinha("");
+
+    if (Console.ReadLine().ToLower() != "s")
+        break;
+
+    Console.Clear();
+}
+
+void PerguntaNome()
+{
+    EscreverLinha("Digite o nome do funcionário: ");
+    funcionario.Nome = Console.ReadLine();
+    EscreverLinha("");
+}
+
+void PerguntaSalario()
+{
+    EscreverLinha("Digite o salário do funcionário: ");
+    funcionario.Salario = Convert.ToDecimal(Console.ReadLine());
+    EscreverLinha("");
+}
+
+void PerguntaTotalHorasExtras()
+{
+    EscreverLinha("Digite o total de horas extras: ");
+    funcionario.HorasExtras = Convert.ToDecimal(Console.ReadLine());
+    EscreverLinha("");
+}
+
+void EscreverLinha(string texto) 
+{
+    Console.WriteLine(texto);
+}
+
+
+static void SalvarContracheque(Funcionario funcionario)
+{
+    string json = JsonConvert.SerializeObject(funcionario);
+    string path = @"C:\Contracheques";
+    Directory.CreateDirectory(path);
+    File.WriteAllText(Path.Combine(path, $"{funcionario.Nome}_contracheque.txt"), json);
+    Console.WriteLine("Contracheque salvo com sucesso!");
+}
